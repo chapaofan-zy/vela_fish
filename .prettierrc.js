2351f48b27666333b3b9a320d98c409a67ee4f39
张扬 <zhangyang56@xiaomi.com>
2025-06-24 19:48:31 +0800

    feat: use ts

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,7 +2,6 @@ module.exports = {
   printWidth: 100, // 指定代码长度，超出换行
   tabWidth: 2, // tab 键的宽度
   useTabs: false, // 使用空格替代tab
-  semi: false, // 结尾加上分号
   singleQuote: false, // 使用单引号
   quoteProps: "consistent", // 要求对象字面量属性是否使用引号包裹,(‘as-needed’: 没有特殊要求，禁止使用，'consistent': 保持一致 , preserve: 不限制，想用就用)
   trailingComma: "none", // 不添加对象和数组最后一个元素的逗号
@@ -18,5 +17,6 @@ module.exports = {
       files: "*.ux",
       options: {parser: "vue"}
     }
-  ]
-}
+  ],
+  bracketSpacing: true
+};
